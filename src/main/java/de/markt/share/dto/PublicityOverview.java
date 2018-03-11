1e89b98ab0a57a47eead2303bd5e8ea5d414dc9a
tobi.habitat <tobi.habitat@gmail.com>
2018-03-11 12:14:19 +0100

    add publicityOverview

---
+++ src/main/java/de/markt/share/dto/PublicityOverview.java
@@ -0,0 +1,12 @@
+package de.markt.share.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public class PublicityOverview {
+    public String guiId;
+    public Date insertDate;
+    public String title;
+    public String description;
+    public List<String> tags;
+}
