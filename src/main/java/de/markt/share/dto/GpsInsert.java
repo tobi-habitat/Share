6bf32115a1a4b4d55cfd85e16382b2bea205617d
tobi.habitat <tobi.habitat@gmail.com>
2018-03-13 22:00:42 +0100

    add gps

---
+++ src/main/java/de/markt/share/dto/GpsInsert.java
@@ -0,0 +1,11 @@
+package de.markt.share.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GpsInsert {
+
+    public String androidId;
+    public List<GpsLocation> locations = new ArrayList<>();
+
+}
---
+++ src/main/java/de/markt/share/dto/GpsLocation.java
@@ -0,0 +1,9 @@
+package de.markt.share.dto;
+
+import java.util.Date;
+
+public class GpsLocation {
+    public Date date;
+    public double latitude;
+    public double longitude;
+}
