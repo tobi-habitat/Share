7e25eba87b807ec3cde3db36ac720150773d12e4
tobi.habitat <tobi.habitat@gmail.com>
2018-04-03 16:13:19 +0200

    add account registration

---
+++ src/main/java/de/markt/share/dto/AccountRegistration.java
@@ -0,0 +1,8 @@
+package de.markt.share.dto;
+
+public class AccountRegistration {
+    public String email;
+    public String username;
+    public String password;
+    public Boolean generalStandardTermsAndConditions;
+}
