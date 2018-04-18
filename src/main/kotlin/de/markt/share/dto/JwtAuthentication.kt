75dca6c95296bb8fc1413aa39b2b763827ed67c3
tobi.habitat <tobi.habitat@gmail.com>
2018-04-18 20:04:01 +0200

    add jwt

--- src/main/kotlin/de/markt/share/dto/JwtAuthentication.kt
+++ src/main/kotlin/de/markt/share/dto/JwtAuthentication.kt
@@ -1,4 +1,4 @@
 package de.markt.share.dto
 
-data class JwtAuthenticationRequest(val email: String, val password: String)
+data class JwtAuthenticationRequest(val username: String, val password: String)
 data class JwtAuthenticationResponse(val token: String)
