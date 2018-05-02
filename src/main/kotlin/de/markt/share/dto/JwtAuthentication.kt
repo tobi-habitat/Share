6154ad9d5caebc7ca5af3992bd8e26572d4760eb
tobi.habitat <tobi.habitat@gmail.com>
2018-05-02 18:32:45 +0200

    lastPasswordRest as date

--- src/main/kotlin/de/markt/share/dto/JwtAuthentication.kt
+++ src/main/kotlin/de/markt/share/dto/JwtAuthentication.kt
@@ -1,7 +1,9 @@
 package de.markt.share.dto
 
+import java.util.*
+
 data class JwtAuthenticationRequest(val username: String, val password: String)
 data class JwtAuthenticationResponse(val token: String)
 
-data class JwtUser(val username: String, val email: String, val enabled: Boolean, val authorities: List<Authority>, val lastPasswordReset: Long)
+data class JwtUser(val username: String, val email: String, val enabled: Boolean, val authorities: List<Authority>, val lastPasswordReset: Date)
 data class Authority(val authority: String)
