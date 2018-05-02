52819308e0076ebc6d8f3fb12b01c0426b2fbec2
tobi.habitat <tobi.habitat@gmail.com>
2018-05-02 18:25:27 +0200

    authorities as list

--- src/main/kotlin/de/markt/share/dto/JwtAuthentication.kt
+++ src/main/kotlin/de/markt/share/dto/JwtAuthentication.kt
@@ -3,5 +3,5 @@ package de.markt.share.dto
 data class JwtAuthenticationRequest(val username: String, val password: String)
 data class JwtAuthenticationResponse(val token: String)
 
-data class JwtUser(val username: String, val email: String, val enabled: Boolean, val authorities: Authority, val lastPasswordReset: Int)
+data class JwtUser(val username: String, val email: String, val enabled: Boolean, val authorities: List<Authority>, val lastPasswordReset: Int)
 data class Authority(val authority: String)
