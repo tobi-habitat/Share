5944906eaef833216811b010cef3469bd5544de6
tobi.habitat <tobi.habitat@gmail.com>
2018-05-03 11:07:14 +0200

    add nickname

--- src/main/kotlin/de/markt/share/dto/JwtAuthentication.kt
+++ src/main/kotlin/de/markt/share/dto/JwtAuthentication.kt
@@ -5,5 +5,5 @@ import java.util.*
 data class JwtAuthenticationRequest(val username: String, val password: String)
 data class JwtAuthenticationResponse(val token: String)
 
-data class JwtUser(val username: String, val email: String, val enabled: Boolean, val authorities: List<Authority>, val lastPasswordReset: Date)
+data class JwtUser(val nickname: String, val username: String, val email: String, val enabled: Boolean, val authorities: List<Authority>, val lastPasswordReset: Date)
 data class Authority(val authority: String)
