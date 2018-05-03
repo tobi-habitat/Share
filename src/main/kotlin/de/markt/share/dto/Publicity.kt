2cabcc53ab58bcefd2aab42e0b69742806c41dfc
tobi.habitat <tobi.habitat@gmail.com>
2018-05-03 11:56:37 +0200

    add lat and long

--- src/main/kotlin/de/markt/share/dto/Publicity.kt
+++ src/main/kotlin/de/markt/share/dto/Publicity.kt
@@ -2,4 +2,4 @@ package de.markt.share.dto
 
 import java.util.*
 
-class PublicityPreview(val thumbnail: Byte, val title: String, val text: String, val tags: List<String>, val date: Date)
+class PublicityPreview(val thumbnail: Byte, val title: String, val text: String, val tags: List<String>, val date: Date, val lat: Float, val long: Float)
