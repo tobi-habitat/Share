ea6c5823c93d2e6df279dc9e18fb8df02b63af88
tobi.habitat <tobi.habitat@gmail.com>
2018-05-11 09:31:43 +0200

    add publicity detail

---
+++ src/main/kotlin/de/markt/share/dto/PublicityDetail.kt
@@ -0,0 +1,15 @@
+package de.markt.share.dto
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class PublicityDetail(
+        val image: ByteArray,
+        val thumbnail: ByteArray,
+        val lat: Double,
+        val long: Double,
+        val validFrom: Date,
+        val validTo: Date,
+        val title: String,
+        val description: String,
+        val tags: List<String>)
